7e162d995b4ef8c703665d33951f540b637fb008
tienph18 <tienph@younetgroup.com>
2023-07-20 16:57:27 +0700

    fix: fix android & ios issues for Metafox

--- ios/Tiktok.swift
+++ ios/Tiktok.swift
@@ -4,10 +4,10 @@ import Photos
 
 @objc(Tiktok)
 class Tiktok: UIViewController {
-    
+
   @objc
   func auth(_ callback: @escaping RCTResponseSenderBlock) {
-    let scopes = ["user.info.basic,video.list"] // list your scopes
+    let scopes = ["user.info.basic"] // list your scopes
     let scopesSet = NSOrderedSet(array:scopes)
     let request = TikTokOpenSDKAuthRequest()
     request.permissions = scopesSet
@@ -20,7 +20,7 @@ class Tiktok: UIViewController {
       })
     }
   }
-  
+
   @objc
   func share(_ path: String, callback: @escaping RCTResponseSenderBlock) {
     PHPhotoLibrary.shared().performChanges({
